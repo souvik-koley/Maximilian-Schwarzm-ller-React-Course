34e9d047889c7229b0e74298c9e195fab1f7c9e3
souvikkoley9674 <113795422+souvikkoley9674@users.noreply.github.com>
2023-02-11 13:03:56 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+    return (
+        <div>
+            <h1>Getting Started</h1>
+            <ExpenseItem/>
+        </div>
+    );
+}
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Feb 11th 2023 </div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$140</div>
+            </div>
+        </div>
+    );
+}
+export default ExpenseItem;
