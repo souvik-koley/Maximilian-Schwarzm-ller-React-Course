1cf7c13f2621119a87d5fc2a156551701c2dcfb9
souvikkoley9674 <113795422+souvikkoley9674@users.noreply.github.com>
2023-02-12 19:52:47 +0530

    onClick method implement

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,14 +3,20 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
-const ExpenseItem=(props)=> {
+const ExpenseItem = (props) => {
+    let title =props.title;
+    const clickHandler = () => {
+        title="updated";
+        console.log(title);
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
